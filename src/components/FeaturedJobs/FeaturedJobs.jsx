635cd0a9f8cb0a1fe9f0c9b262b82632353553fd
noushin-nusrat <noushinnusrat21@gmail.com>
2024-03-23 10:07:27 +0600

    added footer section

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 
 const FeaturedJobs = () => {
+
+    const [jobs, setJobs] = useState([]);
+    useEffect(() => {
+        fetch('jobs.json')
+            .then(res => res.json())
+            .then(data => setJobs(data));
+    }, [])
+
     return (
-        <div>
-             <h2 className="text-5xl text-center">Featured Jobs</h2>
+      <div>
+          <div>
+            <h2 className="text-5xl text-center">Featured Jobs: {jobs.length}</h2>
             <p className="text-base text-center p-4">Explore thousands of job opportunities with all the information you need. Its your future.</p>
         </div>
+        <div>
+        {
+            jobs.map(job=><Job key={job.id} job={job}></Job>)
+        }
+       </div>
+      </div>
+       
     );
 };
 
